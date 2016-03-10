89ea7544eaaf7433cc773e8111c320e2f950bccd
dorothym <dorothy.v.moore@gmail.com>
2016-03-10 17:30:59 -0500

    committing before moving on to file writing on AWS

--- server/app/index.js
+++ server/app/index.js
@@ -7,6 +7,7 @@ module.exports = app;
 // Pass our express application pipeline into the configuration
 // function located at server/app/configure/index.js
 require('./configure')(app);
+require('./aws')(app);
 
 // Routes that will be accessed via AJAX should be prepended with
 // /api so they are isolated from our GET /* wildcard.
@@ -32,6 +33,8 @@ app.use(function (req, res, next) {
 app.get('/*', function (req, res) {
     res.sendFile(app.get('indexHTMLPath'));
 });
+
+
 
 // Error catching endware.
 app.use(function (err, req, res, next) {
