bcbaf67679be7d5bfa00cf33dc368adef2912fb4
dorothym <dorothy.v.moore@gmail.com>
2016-03-13 23:24:32 -0400

    upload page looks better. file download is operable on single file page

--- browser/js/files/single.file/single.file.controller.js
+++ browser/js/files/single.file/single.file.controller.js
@@ -1,16 +1,8 @@
-app.controller('FileController', function ($scope, AuthService, SingleFile, oneFile, FilesFactory, ngAudio) {
+app.controller('FileController', function ($scope, AuthService, SingleFile, oneFile, FilesFactory, ngAudio, $stateParams, filepickerService) {
 
-	// ngAudio testing
-
+	$scope.successmessage = $stateParams.successmessage;
 
 	$scope.file = oneFile;
-	// $scope.file.path = "https://cdn.filepicker.io/api/file/" + oneFile._id
-	// console.log("scope file path is ",$scope.file.path)
-    // $scope.audioObject = ngAudio.load($scope.file.path); // returns NgAudioObject
-	// $scope.audioObject = ngAudio.load("https://cdn.filepicker.io/api/file/MckPdgJgQFSoRSkwApCh"); // returns NgAudioObject
-    // $scope.audioObject = ngAudio.load($scope.file.filePath);
-	console.log("file is", $scope.file)
-	console.log("filepath is", $scope.file.filePath)
 	$scope.audioObject = ngAudio.load($scope.file.filePath); // returns NgAudioObject
 
 	$scope.goToTrack = FilesFactory.goToTrack;
@@ -19,5 +11,18 @@ app.controller('FileController', function ($scope, AuthService, SingleFile, oneF
 		return AuthService.isAuthenticated();
 	};
 
- 
+     $scope.downloadFile = function(file) {
+     	var locationOfDot = file.s3key.indexOf('.')
+     	var suggestedExtension = file.s3key.substring(locationOfDot)
+        console.log("in downloadFile",file)
+        filepickerService.exportFile(file.filePath,
+            {
+            suggestedFilename: file.title,
+            extension: suggestedExtension
+            },
+        function(Blob){
+	        console.log(Blob.url);
+	        }
+        );
+    };
 });
